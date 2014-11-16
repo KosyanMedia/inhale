dedcb24e4d8f605f5767d1bc1c18919ac0e9681d
bskaplou <eshikov@mail.ru>
2014-11-16 11:11:28 +0700

    map chart type raw for now but matches our needs

--- q2g.py
+++ q2g.py
@@ -158,6 +158,65 @@ def render_google_chart(cols, rows, chart_type='table',options={}):
     tor = tor + TABLE_TAIL.replace('TYPE', chart_type.capitalize())
     return tor
 
+MAP_HEAD="""
+<html>
+<head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+         <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.css" />
+        <style>.iata{{}}</style>
+</head>
+<body>
+        <div id="map" style="width: 100%; height: 100%"></div>
+        <script src="http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.js"></script>
+        <script>
+                var map = L.map('map', {{zoomControl: false}}).setView([{center}], {zoom});
+                L.tileLayer(
+                    'http://{{s}}.tile.osm.org/{{z}}/{{x}}/{{y}}.png',
+                     {{
+                        id: 'examples.map-i875mjb7'
+                     }}
+                ).addTo(map);
+"""
+
+MAP_POPUP="""
+                L.marker([{coords}], {{icon: L.divIcon({{className: "iata", html: "{iata}", }})}}).addTo(map);
+"""
+
+MAP_LINE="""
+                L.polyline([
+                        [{orig_coords}],
+                        [{dest_coords}]
+                ], {{
+                    color: 'red',
+                    weight: 0.006 * {searches},
+                    opacity: 0.003 * {searches}
+                }}).addTo(map);
+"""
+
+MAP_TAIL="""
+        </script>
+</body>
+</html>
+"""
+
+def render_map(cols, rows, chart_type,options={}):
+    tor = [MAP_HEAD.format(**options)]
+    iatas = {}
+    for l, r  in rows.items():
+        if 'fix_coords' in options:
+            r['orig_coords'] = ','.join(reversed(r['orig_coords'].split(':')))
+            r['dest_coords'] = ','.join(reversed(r['dest_coords'].split(':')))
+        tor.append(MAP_LINE.format(**r))
+        if r['orig'] not in iatas:
+            iatas[r['orig']] = True
+            tor.append(MAP_POPUP.format(coords=r['orig_coords'], iata=r['orig']))
+        if r['dest'] not in iatas:
+            iatas[r['dest']] = True
+            tor.append(MAP_POPUP.format(coords=r['dest_coords'], iata=r['dest']))
+    tor.append(MAP_TAIL.format(**options))
+    return ''.join(tor)
+
 class SvgHandler(RequestHandler):
     @tornado.web.asynchronous
     @tornado.gen.coroutine
@@ -167,10 +226,8 @@ class SvgHandler(RequestHandler):
         secondary = self.get_argument('secondary', '').split(',')
         sparkline = self.get_argument('sparkline', False)
         chart_type = self.get_argument('type', 'line')
-        if chart_type != 'table':
-            chart = chart_types[chart_type]
         time_format = self.get_argument('time_format', '%Y-%m-%d %H:%M:%S').replace('$', '%')
-        self.set_header("Content-Type", 'image/svg+xml' if chart_type not in ('table','gauge') else 'text/html')
+        self.set_header("Content-Type", 'image/svg+xml' if chart_type not in ('table','gauge', 'map') else 'text/html')
         try:
             response = yield select(ops.db_host, ops.db_port, series, query, login=ops.db_login, password=ops.db_password)
             x_axis = self.get_argument('x', 'time')
@@ -187,7 +244,7 @@ class SvgHandler(RequestHandler):
                         value.font_family = font
                     elif name == 'range':
                         value = sorted(list(map(float, value.split(','))))
-                    elif match(r"[\d-]+", value):
+                    elif match(r"^[\d-]+$", value):
                         value = int(value)
                     chart_params[name] = value
             if x_axis == 'time':
@@ -197,7 +254,10 @@ class SvgHandler(RequestHandler):
 
             if chart_type in ('table', 'gauge'):
                 self.write(render_google_chart([x_axis] + list(cols), rows, chart_type=chart_type, options=chart_params))
+            elif chart_type == 'map':
+                self.write(render_map([x_axis] + list(cols), rows, chart_type=chart_type, options=chart_params))
             else:
+                chart = chart_types[chart_type]
                 bar_chart = chart(**chart_params)
                 bar_chart.x_labels = x_labels
                 for col in cols:
